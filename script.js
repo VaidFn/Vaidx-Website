29546f41351dc3cd6df0cadf6d7f87a2ea613ee2
VaidFn <amautomato.lev@gmail.com>
2025-06-01 17:59:28 +0200

    good

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('learnMoreBtn').addEventListener('click', () => {
-  alert("La NumWorks est idéale pour les élèves de lycée, avec Python intégré et une interface intuitive !");
+document.getElementById('myButton').addEventListener('click', () => {
+  alert("Tu as cliqué sur le bouton !");
 });
