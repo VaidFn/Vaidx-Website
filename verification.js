562a3e216d3b0e4c73b9e197d4669b909ecc5ed6
Vaidx <amautomato.lev@gmail.com>
2025-06-04 22:56:37 +0200

    Update verification.js

--- verification.js
+++ verification.js
@@ -1,1 +1,9 @@
+// verification.js
+function checkPseudoValide() {
+    if (!sessionStorage.getItem('pseudoValide')) {
+        window.location.href = 'index.html'; // Rediriger vers la page de vérification
+    }
+}
 
+// Appeler la fonction pour vérifier le pseudo au chargement de la page
+window.onload = checkPseudoValide;
