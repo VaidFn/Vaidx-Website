a89b7171284806320df8204cee86fb170af1e60a
Vaidx <amautomato.lev@gmail.com>
2025-06-04 22:59:48 +0200

    Delete verification.js

--- verification.js
+++
@@ -1,9 +0,0 @@
-// verification.js
-function checkPseudoValide() {
-    if (!sessionStorage.getItem('pseudoValide')) {
-        window.location.href = 'index.html'; // Rediriger vers la page de vérification
-    }
-}
-
-// Appeler la fonction pour vérifier le pseudo au chargement de la page
-window.onload = checkPseudoValide;
